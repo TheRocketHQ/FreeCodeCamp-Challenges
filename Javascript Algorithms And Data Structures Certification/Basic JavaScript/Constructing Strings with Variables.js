95b8774d67590f523a2dc149a5185925fc3796de
Francisco Mateus <gtaskfm@gmail.com>
2018-09-03 21:29:04 +0200

    Constructing Strings with Variables

---
+++ Javascript Algorithms And Data Structures Certification/Basic JavaScript/Constructing Strings with Variables.js
@@ -0,0 +1,7 @@
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "freeCodeCamp";
+var myStr = "Hello, our name is " + myName + ", how are you?";
