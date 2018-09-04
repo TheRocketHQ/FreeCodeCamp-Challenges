08404f6a8d2c388d920cee5a2afaf3047647e77c
Francisco Mateus <gtaskfm@gmail.com>
2018-09-04 20:39:20 +0200

    Understanding Undefined Value returned from a Function

---
+++ Javascript Algorithms And Data Structures Certification/Basic JavaScript/Understanding Undefined Value returned from a Function.js
@@ -0,0 +1,18 @@
+// Example
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+
+var sum = 3;
+
+function addFive() {
+    sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
